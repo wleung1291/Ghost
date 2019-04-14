022e11af6c868f140a241d238c514c72ea1688bf
Wilson <wleung1291@gmail.com>
2019-04-13 21:34:46 -0700

    Added comment

--- play_ghost.rb
+++ play_ghost.rb
@@ -5,17 +5,20 @@ class Play_ghost
     # manual player instantiation implementation
     # play = Ghost.new(  Player.new("Person 1"), Player.new("Person 2"), )
     
-    # multiple players
-    puts "Number of players: "
-    num_players = gets.chomp.to_i
-    players = []
-    (1..num_players).each do |i|
-        puts "Enter Name for Player #{i}:" 
-        players << Player.new("#{gets.chomp}") 
-    end
+    if __FILE__ == $PROGRAM_NAME
+        puts "Number of players: "
+        num_players = gets.chomp.to_i
+        players = []
 
-    play = Ghost.new( players )   
-    play.populate_dict()
-    play.play_round() 
+        # create an array of Player objects
+        (1..num_players).each do |i|
+            puts "Enter Name for Player #{i}:" 
+            players << Player.new("#{gets.chomp}") 
+        end
+
+        play = Ghost.new( players )   
+        play.populate_dict()
+        play.play_round() 
+    end
 
 end # end of Play_ghost class
